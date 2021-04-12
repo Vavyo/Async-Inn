5979481aefd31bac4f18c0d8e564945f2baa3c9a
Vavyo <vavyo2121@gmail.com>
2021-04-12 18:11:53 -0500

    HotelRoom Interface

---
+++ WebApplication1/Data/Interfaces/IHotelRoomRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Models;
+
+namespace WebApplication1.Data.Interfaces
+{
+    public interface IHotelRoomRepository
+    {
+        Task<IEnumerable<HotelRoom>> GetAllHotelRooms();
+
+        Task CreateHotelRoom(HotelRoom hotelRoom);
+
+        Task<HotelRoom> GetHotelRoom(int id, int number);
+
+        Task<bool> UpdateHotelRoom(HotelRoom hotelRoom);
+
+        Task DeleteHotelRoom(HotelRoom hotelRoom);
+
+    }
+}
