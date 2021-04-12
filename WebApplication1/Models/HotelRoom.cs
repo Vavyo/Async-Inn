ce8e9be037b8eeb375385be62af290ac0d414573
Vavyo <vavyo2121@gmail.com>
2021-04-12 18:07:12 -0500

    HotelRoom Model

---
+++ WebApplication1/Models/HotelRoom.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class HotelRoom
+    {
+        public int HotelId { get; set; }
+        public int RoomNumber { get; set; }
+        public int RoomId { get; set; }
+        public decimal Rate { get; set; }
+        public bool PetFriendly { get; set; }
+    }
+}
