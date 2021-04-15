f95a875417ca0a811aba2dbd81f371c39dfc84c9
Vavyo <vavyo2121@gmail.com>
2021-04-15 05:29:07 -0500

    auto migrate

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.Data;
 
 namespace WebApplication1
 {
@@ -13,8 +16,32 @@ namespace WebApplication1
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            UpdateDatabase(host.Services);
+
+            host.Run();
         }
+
+        private static void UpdateDatabase(IServiceProvider services)
+        {
+            using (var serviceScope = services.CreateScope())
+            {
+                using (var db = serviceScope.ServiceProvider.GetService<AsyncInnDbContext>())
+                {
+                    db.Database.Migrate();
+                }
+            }
+        }
+
+        /*
+         * The above can also be written like this, which is far cleaner, yet less explicit, without the nesting
+         *
+         * using var serviceScope = services.CreateScope();
+         * using var db = serviceScope.ServiceProvider.GetService<SchoolDbContext>()
+         * db.Database.Migrate();
+         *
+         */
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
